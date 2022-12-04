1a06a50cb214609bc4429f8bbc1efa8f457519af
Ethan Welsh <ewelsh@google.com>
2022-12-03 21:57:57 -0800

    Solved day 4

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -10,6 +10,7 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Control.Monad (void)
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
@@ -20,20 +21,59 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+
+rangeParser :: Parser Range
+rangeParser = do
+    a <- decimal
+    void $ char '-'
+    b <- decimal
+    return (a, b)
+
+pairParser :: Parser Pair
+pairParser = do
+    a <- rangeParser
+    void $ char ','
+    b <- rangeParser
+    return (a, b)
+
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = pairParser `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
 
-type OutputA = Void
+type Range = (Int, Int)
 
-type OutputB = Void
+type Pair = (Range, Range)
+
+type Input = [Pair]
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+
+rangeToList :: Range -> [Int]
+rangeToList (a, b) = [a..b]
+
+fullyContains :: Pair -> Bool
+fullyContains (a, b) = let
+    al = rangeToList a
+    bl = rangeToList b
+    isSubsetOf needle haystack = all (`elem` haystack) needle
+    in isSubsetOf al bl || isSubsetOf bl al
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = length $ filter (fullyContains) input
 
 ------------ PART B ------------
+
+overlaps :: Pair -> Bool
+overlaps (a, b) = let
+    al = rangeToList a
+    bl = rangeToList b
+    containsAny needle haystack = any (`elem` haystack) needle
+    in containsAny al bl
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = length $ filter (overlaps) input
