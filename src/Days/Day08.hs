0636d20624d5e7dc1c33b49642eef8a5380df1c6
Ethan Welsh <ewelsh@google.com>
2022-12-08 06:47:37 -0800

    Solve day 8

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -21,19 +21,89 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (many1 (notChar '\n')) `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Point = (Int, Int)
 
-type OutputA = Void
+type Trees = [String]
 
-type OutputB = Void
+type Input = Trees
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+
+range :: Int -> Int -> [Int]
+range start end
+    | start < end  = [start..end]
+    | start > end  = [start, start - 1 .. end]
+    | start == end = []
+
+get :: Trees -> Point -> Char
+get ts (r, c) = (ts !! r) !! c
+
+getHeight :: Trees -> Int
+getHeight ts = length ts
+
+getWidth :: Trees -> Int
+getWidth ts = length (ts !! 0)
+
+getLinesOfSite :: Trees -> Point -> [[Point]]
+getLinesOfSite ts (r, c) = let
+    height = (getHeight ts) - 1
+    width = (getWidth ts) - 1
+    up = zip    (range r 0)      (repeat c)
+    down = zip  (range r height) (repeat c)
+    left = zip  (repeat r)       (range c 0)
+    right = zip (repeat r)       (range c width)
+    in [up, down, left, right]
+
+isStrictlyDecreasing :: Ord a => [a] -> Bool
+isStrictlyDecreasing [] = True
+isStrictlyDecreasing (x:xs) = all (<x) xs
+
+isVisible :: Trees -> Point -> Bool
+isVisible ts p = let
+    linesOfSitePoints = getLinesOfSite ts p                   :: [[Point]]
+    linesOfSiteValues = map (map (get ts)) linesOfSitePoints  :: [[Char]]
+    isDecreasing = map isStrictlyDecreasing linesOfSiteValues :: [Bool]
+    in or isDecreasing
+
+getAllPoints :: Trees -> [Point]
+getAllPoints ts = let
+    height = (getHeight ts) - 1
+    width = (getWidth ts) - 1
+    in  [(r, c) | r <- [0..height], c <- [0..width]]
+
+countWhere :: (a -> Bool) -> [a] -> Int
+countWhere pred xs = length $ filter pred xs
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA trees = let
+    allPoints = getAllPoints trees
+    in countWhere (isVisible trees) allPoints
 
 ------------ PART B ------------
+
+countDecreasingFrom :: Ord a => a -> [a] -> Int
+countDecreasingFrom _ [] = 0
+countDecreasingFrom t (x:xs) = if x >= t then 1 else 1 + countDecreasingFrom t xs
+
+countDecreasing :: Ord a => [a] -> Int
+countDecreasing [] = 0
+countDecreasing (x:xs) = countDecreasingFrom x xs
+
+getScore :: Trees -> Point -> Int
+getScore ts p = let
+    linesOfSitePoints = getLinesOfSite ts p                   :: [[Point]]
+    linesOfSiteValues = map (map (get ts)) linesOfSitePoints  :: [[Char]]
+    in product $ map countDecreasing linesOfSiteValues
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB ts = let
+    allPoints = getAllPoints ts :: [Point]
+    allScores = map (getScore ts) allPoints :: [Int]
+    in maximum allScores
