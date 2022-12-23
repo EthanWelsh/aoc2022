6486db3173e362783d20a74f6b4cee2a2d6c027a
Ethan Welsh <ewelsh@google.com>
2022-12-23 14:28:24 -0800

    Solve day 21 part 1

--- src/Days/Day21.hs
+++ src/Days/Day21.hs
@@ -10,7 +10,9 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
-
+import Control.Monad (void)
+import Control.Applicative ((<|>))
+import Data.Functor (($>))
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
@@ -20,20 +22,91 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+
+nameParser :: Parser String
+nameParser = many1 letter
+
+numberParser :: Parser (Map String Operation)
+numberParser = do
+    name <- nameParser
+    void $ string ": "
+    num <- decimal
+    return $ Map.fromList [(name, Number num)]
+
+binaryOperationPartsParser :: Parser (String, Char, String)
+binaryOperationPartsParser = do
+    name1 <- nameParser
+    void $ string " "
+    op <- anyChar 
+    void $ string " "
+    name2 <- nameParser
+    return (name1, op, name2)
+
+binaryOperationParser :: Parser (Map String Operation)
+binaryOperationParser = do
+    name <- nameParser
+    void $ string ": "
+    (a, op, b) <- binaryOperationPartsParser
+    let operation = case op of 
+                        '+' -> Plus a b
+                        '-' -> Minus a b
+                        '*' -> Multiply a b
+                        '/' -> Divide a b
+    return $ Map.fromList [(name, operation)]
+
+
+
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+    ops <- (numberParser <|> binaryOperationParser) `sepBy` endOfLine
+    return $ Map.unions ops
+
 
 ------------ TYPES ------------
-type Input = Void
+data Operation = Number Int | Plus String String | Minus String String | Multiply String String | Divide String String deriving (Eq, Ord, Show)
 
-type OutputA = Void
+type Input = Map String Operation
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = String
 
 ------------ PART A ------------
+solve :: Map String Operation -> String -> Int
+solve m name = let
+    op = m Map.! name
+    result = case op of
+                Number n -> n
+                Plus a b ->     (solve m a) +     (solve m b)
+                Minus a b ->    (solve m a) -     (solve m b)
+                Multiply a b -> (solve m a) *     (solve m b)
+                Divide a b ->   (solve m a) `div` (solve m b)
+    in result
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = solve input "root"
 
 ------------ PART B ------------
+
+getTwoSides :: Operation -> (String, String)
+getTwoSides (Plus a b) = (a, b)
+getTwoSides (Minus a b) = (a, b)
+getTwoSides (Multiply a b) = (a, b)
+getTwoSides (Divide a b) = (a, b)
+
+test :: Map String Operation -> Int -> Bool
+test m human = let
+    newMap = Map.insert "humn" (Number human) m
+    (a, b) = getTwoSides $ m Map.! "root"
+    leftSide = solve newMap a
+    rightSide = solve newMap b
+    in leftSide == rightSide
+
+findEqualityPoint :: Map String Operation -> Int
+findEqualityPoint m = fromJust $ find (test m) [1..]
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = let
+    in show $ findEqualityPoint input
+    
+    --solve input "root"
